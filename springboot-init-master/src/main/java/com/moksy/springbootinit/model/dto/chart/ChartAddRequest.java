00fabbe434c0edfacbff2061b3b906ccc163819a
moxi <1604979676@qq.com>
2024-10-19 08:28:45 +0800

    第五次修改后可以运行的版本

---
+++ springboot-init-master/src/main/java/com/moksy/springbootinit/model/dto/chart/ChartAddRequest.java
@@ -0,0 +1,48 @@
+package com.moksy.springbootinit.model.dto.chart;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 创建请求
+ */
+@Data
+public class ChartAddRequest implements Serializable {
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 分析目标
+     */
+    private String goal;
+
+    /**
+     * 图表数据
+     */
+    private String chartData;
+
+    /**
+     * 图表类型
+     */
+    private String chartType;
+
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ springboot-init-master/src/main/java/com/moksy/springbootinit/model/dto/chart/ChartQueryRequest.java
@@ -0,0 +1,48 @@
+package com.moksy.springbootinit.model.dto.chart;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.moksy.springbootinit.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 查询请求
+ *
+ * 
+ * 
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ChartQueryRequest extends PageRequest implements Serializable {
+
+
+    private long id;
+    /**
+     * 分析目标
+     */
+    private String goal;
+
+    /**
+     * 图表数据
+     */
+    private String chartData;
+
+    /**
+     * 图表类型
+     */
+    private String chartType;
+    /*
+    * 用户ID
+    * */
+
+    private long userId;
+
+    private static final long serialVersionUID = 1L;
+}
